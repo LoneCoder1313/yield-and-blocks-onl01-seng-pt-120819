c9ea9ab3742005789a8f0e7d48936489ea02bb0d
Ricky dildy <rdildy92@gmail.com>
2019-12-22 20:52:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,5 @@
-def hello_t(names)
-        i = 0 
-    names.each do |names|
-    while i < names.length
-      yield names[i]
-    i = i + 1
-  end
-  names 
-end
+def hello_t(array)
+  
+end 
+   
 
