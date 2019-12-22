7475bf313a0403d900b4cc7ac32438004b30f1e5
Ricky dildy <rdildy92@gmail.com>
2019-12-22 21:20:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,9 +2,9 @@ def hello_t(names)
   i = 0 
   names.each_with_index { |names| puts names}
 end 
-  while i < names.length 
-  yield names[i]
-  i = i +1 
 end 
-end
+#   while i < names.length 
+#   yield names[i]
+#   i = i +1 
+# end 
 
