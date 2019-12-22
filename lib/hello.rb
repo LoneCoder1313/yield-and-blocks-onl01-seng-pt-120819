dcb01e91e3ef67c7e1daca8fa451ecd7ed892562
Ricky dildy <rdildy92@gmail.com>
2019-12-22 19:48:16 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,12 @@
 def hello_t(names)
   names.each do |names|
-    puts names
+    puts names.yield
 end
 end 
 
 
 def hello_t(other_names)
   other_names.each do |names|
-    puts other_names
+    puts other_names.yield
 end 
 end 
