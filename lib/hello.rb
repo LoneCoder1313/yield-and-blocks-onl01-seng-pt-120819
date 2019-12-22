f60a6f30fba5ac988f4dbe16fcb6a74fa96e83ce
Ricky dildy <rdildy92@gmail.com>
2019-12-22 19:54:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,20 @@
 def hello_t(names)
-  i = 0 
-while i < names.length
-    yield names[i]
-    i = i + 1
-  end
-end
 
-def hello_t(other_names)
-  other_names.each do |names|
-    puts other_names
-end 
-end 
+
+
+
+
+
+
+#   i = 0 
+# while i < names.length
+#     yield names[i]
+#     i = i + 1
+#   end
+# end
+
+# def hello_t(other_names)
+#   other_names.each do |names|
+#     puts other_names
+# end 
+# end 
