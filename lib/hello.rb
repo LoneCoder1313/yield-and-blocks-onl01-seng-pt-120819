ff1cfe92ea4fedaf6df7f01afe971faaf34f1bdd
Ricky dildy <rdildy92@gmail.com>
2019-12-22 21:29:29 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,15 @@
 def hello_t(names)
+  if block_given?
   i = 0 
  
   while i < names.length 
-  yield names[i]
+  yield (names[i])
   i = i +1 
 end 
     names 
+  else 
+    puts "Hey! No block was given!"
+  end 
 end 
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
@@ -14,13 +18,3 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   end
 end
 
-def hello_t(array)
-  i = 0
-
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
-
-  array
-end
