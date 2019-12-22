0580d5b270a6047bbe8ab0fe52ff844c313031f5
Ricky dildy <rdildy92@gmail.com>
2019-12-22 19:55:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,24 +1,20 @@
 def hello_t(names)
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("")
-    puts "Hi, #{name}"
+
+
+
+
+
+
+
+  i = 0 
+while i < names.length
+    yield names[i]
+    i = i + 1
   end
 end
 
-
-
-
-
-
-#   i = 0 
-# while i < names.length
-#     yield names[i]
-#     i = i + 1
-#   end
-# end
-
-# def hello_t(other_names)
-#   other_names.each do |names|
-#     puts other_names
-# end 
-# end 
+def hello_t(other_names)
+  other_names.each do |names|
+    puts other_names
+end 
+end 
